c10c605d6b4dd27a4285e663d9cbfb8b79d6fe12
Tong, Irene <irentong@amazon.com>
2016-11-29 16:48:41 -0800

    Update per feedback.

--- app/src/main/java/udacity/irene/com/appportfolio/MainActivity.java
+++ app/src/main/java/udacity/irene/com/appportfolio/MainActivity.java
@@ -12,50 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        findViewById(R.id.button_build_it_bigger).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getBaseContext(), String.format(getString(R.string.this_will_launch), getString(R.string.build_it_bigger)),
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-        findViewById(R.id.button_stock_hawk).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getBaseContext(), String.format(getString(R.string.this_will_launch), getString(R.string.stock_hawk)),
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-        findViewById(R.id.button_capstone).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getBaseContext(), String.format(getString(R.string.this_will_launch), getString(R.string.capstone_my_own_app)),
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-        findViewById(R.id.button_go_ubiquitous).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getBaseContext(), String.format(getString(R.string.this_will_launch), getString(R.string.go_ubiquitous)),
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-        findViewById(R.id.button_make_material).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getBaseContext(), String.format(getString(R.string.this_will_launch), getString(R.string.make_your_app_material)),
-                        Toast.LENGTH_SHORT).show();
+    public void showToast(View v) {
+        Toast.makeText(getBaseContext(), String.format(getString(R.string.this_will_launch), ((Button)v).getText()),
+                Toast.LENGTH_SHORT).show();
 
-            }
-        });
-        findViewById(R.id.button_popular_movies).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getBaseContext(), String.format(getString(R.string.this_will_launch), getString(R.string.popular_movies)),
-                        Toast.LENGTH_SHORT).show();
-
-            }
-        });
     }
 }
